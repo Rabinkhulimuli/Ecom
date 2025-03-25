8602f00038ffd562a58dd31f4e6b451aa40699ae
Rabinkhulimuli <131673248+Rabinkhulimuli@users.noreply.github.com>
2025-03-25 18:38:21 +0545

    fdjnl

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -3,7 +3,7 @@
 import { FcGoogle } from "react-icons/fc";
 export const SignIn = () => {
 const handleLogin=()=> {
-  window.location.href=`https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&redirect_uri=${process.env.REDIRECT_URL}/api/auth/google/redirect&scope=profile%20email&client_id=801191979878-14auvuc2kbkie7r98ea8fpb3n7picmou.apps.googleusercontent.com&service=lso&o2v=2&ddm=1&flowName=GeneralOAuthFlow`
+  window.location.href=`https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&redirect_uri=${process.env.REDIRECT_URL||"https://ecomapi-kom2.onrender.com"}/api/auth/google/redirect&scope=profile%20email&client_id=801191979878-14auvuc2kbkie7r98ea8fpb3n7picmou.apps.googleusercontent.com&service=lso&o2v=2&ddm=1&flowName=GeneralOAuthFlow`
 }
   return (
     <button
